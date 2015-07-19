5f5ddedab74aba4bd12facc25fdcb9f094729af6
geekgeek <solq87@gmail.com>
2015-07-19 04:12:53 +0200

    Create sqlite_create_select_read.java

---
+++ sqlite_create_select_read.java
@@ -0,0 +1,61 @@
+//https://en.wikibooks.org/wiki/Java_JDBC_using_SQLite/Connecting
+
+package javaapplication1;
+import java.sql.Connection;
+import java.sql.Statement;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.DatabaseMetaData;
+ 
+public class JavaApplication1 {
+
+    
+    public static void main(String[] args) throws Exception{
+
+        // register the driver 
+        String sDriverName = "org.sqlite.JDBC";
+        Class.forName(sDriverName);
+ 
+        // now we set up a set of fairly basic string variables to use in the body of the code proper
+        String sTempDb = "databasetest.txt";
+        String sJdbc = "jdbc:sqlite";
+        String sDbUrl = sJdbc + ":" + sTempDb;
+        // which will produce a legitimate Url for SqlLite JDBC :
+        // jdbc:sqlite:hello.db
+        int iTimeout = 30;
+        String sMakeTable = "CREATE TABLE dummy (id numeric, response text)";
+        String sMakeInsert = "INSERT INTO dummy VALUES(1,'Hello from the database')";
+        String sMakeSelect = "SELECT response from dummy";
+ 
+        // create a database connection
+        Connection conn = DriverManager.getConnection(sDbUrl);
+        try {
+            Statement stmt = conn.createStatement();
+            try {
+                stmt.setQueryTimeout(iTimeout);
+                stmt.executeUpdate( sMakeTable );
+                stmt.executeUpdate( sMakeInsert );
+                ResultSet rs = stmt.executeQuery(sMakeSelect);
+                try {
+                    while(rs.next())
+                        {
+                            String sResult = rs.getString("response");
+                            System.out.println(sResult);
+                        }
+                } finally {
+                    try { rs.close(); } catch (Exception ignore) {}
+                }
+            } finally {
+                try { stmt.close(); } catch (Exception ignore) {}
+            }
+        } finally {
+            try { conn.close(); } catch (Exception ignore) {}
+        }
+    }
+ 
+}
+ 
+
+  
+
