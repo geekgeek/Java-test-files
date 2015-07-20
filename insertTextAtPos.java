005890e6dee91cdfb2ec7c0fee9ed900148268f1
geekgeek <solq87@gmail.com>
2015-07-20 02:15:41 +0200

    Create insertTextAtPos.java

---
+++ insertTextAtPos.java
@@ -0,0 +1,6 @@
+    //Insert text when clicking at the button at the current selected position in jtextarea
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {                                         
+        // TODO add your handling code here:
+        //button 2
+       this.jTextArea3.insert("text", this.jTextArea3.getCaretPosition());
+    }   
