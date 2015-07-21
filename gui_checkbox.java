4bfc1be9a55d8c0ff2bc8f8c9adb5f3569b45686
geekgeek <solq87@gmail.com>
2015-07-21 08:08:58 +0200

    Create gui_checkbox.java

---
+++ gui_checkbox.java
@@ -0,0 +1,9 @@
+        
+        //JAVA CHECKBOX / RADIOBUTTON
+        if (this.jRadioButton1.isSelected()){
+            System.out.print("is selected");
+            
+        }
+        if (!this.jRadioButton1.isSelected()){
+            System.out.print("is not selected");
+        } 
