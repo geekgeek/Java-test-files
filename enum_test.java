1b70737b20cdc8df067bd356bd1e32d25c5cf5ef
geekgeek <solq87@gmail.com>
2015-07-24 16:03:32 +0200

    Create enum_test.java

---
+++ enum_test.java
@@ -0,0 +1,7 @@
+    public enum PLang{JAVA,PYTHON,SQL};
+    
+    public static void main(String[] args) {
+        
+        PLang bestLanguage = PLang.JAVA;
+        System.out.print(bestLanguage.toString());
+   }
