68d28668a40c6a0efadc34c3390b5b1707ad3c89
geekgeek <solq87@gmail.com>
2015-07-24 07:57:55 +0200

    Create system_info.java

---
+++ system_info.java
@@ -0,0 +1,6 @@
+//http://stackoverflow.com/questions/2861592/possible-to-run-jar-file-on-any-os
+//Get system info
+import java.util.Properties;
+
+    Properties props = System.getProperties();
+    props.list(System.out);
